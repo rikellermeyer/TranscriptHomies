5f8a612f097cf48cd0838718608eebbecd50d200
CarosGiHub <carolineharrer@gmx.de>
2025-10-25 16:01:56 -0400

    added definition

--- Caroline/dictionary_for_visualistion_final.py
+++ Caroline/dictionary_for_visualistion_final.py
@@ -7,31 +7,34 @@ gene_identifier = "gene_id"
 ends_with_experimental = "CP"
 ends_with_control = "C"
 
+def make_dictionary_from_raw_data_for_visualisation ():
+
 #create variable datafile to read the text document with Panda
 # sep="\s+" is a regular expression (regex) that tells pandas how to split the columns when reading the file
 #s= means one or more white spaces (dummy data set may have more than one, had issue running it)
 #engine = python is needed when regular expression is needed for reading the document (here s+)
-df = pd.read_csv(filename, sep="\t", engine="python")
+    df = pd.read_csv(filename, sep="\t", engine="python")
 
 #check headers to understand how to organise experimental versus control columns
-print(df.columns.tolist())
+    #  print(df.columns.tolist())
 
 #remove any white space in column names that may interfere with naming
-df.columns = df.columns.str.strip()
+    df.columns = df.columns.str.strip()
 
 #divide it into two different data tables to run for loop: control (normal) & experimental (cancer)
 #using panda package you can merge the gene_ID as first column with any columns that contain sample identifier
 
-columns_to_keep_in_table_control = [gene_identifier] + [col for col in df.columns if col.endswith(ends_with_control)]
-columns_to_keep_in_table_experimental = [gene_identifier] + [col for col in df.columns if col.endswith(ends_with_experimental)]
+    columns_to_keep_in_table_control = [gene_identifier] + [col for col in df.columns if col.endswith(ends_with_control)]
+    columns_to_keep_in_table_experimental = [gene_identifier] + [col for col in df.columns if col.endswith(ends_with_experimental)]
 
 #df slices the DataFrame to keep only specific columns, that are here listed as columns_to_keep_in_table_experimental and columns_to_keep_in_table_control
-dummy_list_experimental = df[columns_to_keep_in_table_experimental]
-dummy_list_control = df[columns_to_keep_in_table_control]
+    dict_list_experimental = df[columns_to_keep_in_table_experimental]
+    dict_list_control = df[columns_to_keep_in_table_control]
+    return dict_list_experimental, dict_list_control
 
 # Display or use the trimmed DataFrame
-print(dummy_list_experimental)
-print(dummy_list_control)
+print(dict_list_experimental)
+print(dict_list_control)
 
 #make a list in a dictionary: gene_id as key, list of counts of each sample as corresponding value
 #it iterates through all columns that are not "g_IDs" and adds to list
